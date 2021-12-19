d566d69eed113c543dc45b989b50463708ee2860
jonatastonholo <rtjonatas@gmail.com>
2021-12-18 22:32:06 -0300

    - updated git-ignore - created .env.development - implemented financial dependents crud
    Signed-off-by: jonatastonholo <rtjonatas@gmail.com>

---
+++ src/service/financialDependent.service.ts
@@ -0,0 +1,18 @@
+import {IFinancialDependent} from "../domain/IFinancialDependent";
+import client from "../clients/crhono-simples-api/financialDependent.client"
+
+async function findAll() : Promise<IFinancialDependent[] | undefined> {
+  return await client.findAll();
+}
+
+async function remove(financialDependentId: string) : Promise<IFinancialDependent> {
+  return await client.remove(financialDependentId);
+}
+
+async function save(financialDependent: IFinancialDependent) : Promise<IFinancialDependent> {
+  console.log(financialDependent)
+  if (!financialDependent.id) return await client.create(financialDependent);
+  return await client.update(financialDependent);
+}
+
+export default {findAll, remove, save}
